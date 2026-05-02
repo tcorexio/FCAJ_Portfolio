c8a7a30068e41f5078eba21a6e14bcbbaf0cfadf
trongnt <nttroq2101@gmail.com>
2026-05-02 15:21:49 +0700

    Fix react-router-dom dependency and routing import

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hei-FCAJ-intership-report/',
+  base: "/FCAJ_Portfolio/",
   define: {
     __BUILD_DATE__: JSON.stringify(
-      new Date().toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/\//g, '/')
+      new Date()
+        .toLocaleDateString("en-GB", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+        })
+        .replace(/\//g, "/"),
     ),
   },
-})
+});
